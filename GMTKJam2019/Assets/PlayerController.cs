c0f13de0ad156bd8defd8af878f5e535c1a29c06
Vitor <vitormarqueslobo@gmail.com>
2019-08-02 23:20:40 +0100

    Aiming sort of

--- GMTKJam2019/Assets/PlayerController.cs
+++ GMTKJam2019/Assets/PlayerController.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 10f;
+    public bool facingRight = true;
+    public Transform player;
+    public Transform crosshair;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +18,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"),Input.GetAxis("Vertical"),0);
+        transform.position += movement * Time.deltaTime * speed;
+        //crosshair.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);   
+
         
+        
+
     }
 }
