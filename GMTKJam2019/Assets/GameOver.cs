3dde204121ba3eeddc76b36a07a6806bb5911203
joaoamante95 <32554789+joaoamante95@users.noreply.github.com>
2019-08-04 19:43:36 +0100

    Game over

---
+++ GMTKJam2019/Assets/GameOver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void Back2Menu()
+    {
+        SceneManager.LoadScene("Menu");
+
+    }
+    public void Replay()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+}
--- GMTKJam2019/Assets/Scripts/GameManager.cs
+++ GMTKJam2019/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -53,10 +54,20 @@ public class GameManager : MonoBehaviour
 
         enemyList = GameObject.Find("EnemySpawner").transform;
 
-        for (int i = 0; i <= enemyList.childCount; i++)
+        for (int i = 0; i < enemyList.childCount; i++)
         {
             enemyList.GetChild(i).GetComponent<EnemyController>().speed = 0;
         }
 
+        Invoke("ResetScene", 2f);
+
+
     }
+
+    void ResetScene()
+    {
+        Debug.Log("resetting");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
 }
